dcee15a4b6e124fd80e71f9d8b6c9985c0ef1875
arcodez <abelileitor123@gmail.com>
2021-02-12 08:41:18 -0430

    update

--- pages/_document.js
+++ pages/_document.js
@@ -5,13 +5,13 @@ class MyDocument extends Document {
     return (
       <Html lang="es">
         <Head>
-        <link rel="stylesheet" href="/css/About.css"/>
-        <link rel="stylesheet" href="/css/Contact.css"/>
-        <link rel="stylesheet" href="/css/Footer.css"/>
-        <link rel="stylesheet" href="/css/Home.css"/>
-        <link rel="stylesheet" href="/css/Projects.css"/>
-        <link rel="stylesheet" href="/css/Skills.css"/>
-        <link rel="stylesheet" href="/css/ContactForm.css"/>
+          <link rel="stylesheet" href="/css/About.css" />
+          <link rel="stylesheet" href="/css/Contact.css" />
+          <link rel="stylesheet" href="/css/Footer.css" />
+          <link rel="stylesheet" href="/css/Home.css" />
+          <link rel="stylesheet" href="/css/Projects.css" />
+          <link rel="stylesheet" href="/css/Skills.css" />
+          <link rel="stylesheet" href="/css/ContactForm.css" />
           <link
             rel="stylesheet"
             type="text/css"
@@ -22,14 +22,10 @@ class MyDocument extends Document {
             defer
             src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"
           ></script>
-          <script src="https://unpkg.com/scrollreveal"></script>
         </Head>
         <body>
           <Main />
           <NextScript />
-          <script>
-          ScrollReveal().reveal('.App')
-          </script>
         </body>
       </Html>
     );
